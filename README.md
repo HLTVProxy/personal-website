b243b369ac43e80a99277e1698116e24da85f43d
Sam Ho <twhltvproxy@gmail.com>
2023-09-12 14:12:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal website
+
+Built by nextra
---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>Sam Ho is 32</span>,
+  project: {
+    link: 'https://github.com/HLTVProxy/personal-website',
+  },
+  docsRepositoryBase: 'https://github.com/HLTVProxy/personal-website',
+  footer: {
+    text: 'Built by Nextra',
+  },
+};
+
+export default config;
